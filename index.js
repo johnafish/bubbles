5bb361056411bce11ebda6e1503bba534ee12e3b
John Fish <john@johnafish.ca>
2021-12-21 05:21:47 -0500

    Update Canvas Appearance
    Canvas now centered on the page and has a sexy box shadow.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var c = document.getElementById("bubblecanvas");
 var ctx = c.getContext("2d");
+const WHITE = "rgb(255,255,255)";
+const BLACK = "rgb(0,0,0)";
+
+
+ctx.fillStyle = WHITE;
+ctx.fillRect(0, 0, c.width, c.height);
+
+ctx.strokeStyle = BLACK;
 ctx.beginPath();
 ctx.arc(95, 50, 40, 0, 2 * Math.PI);
 ctx.stroke();
